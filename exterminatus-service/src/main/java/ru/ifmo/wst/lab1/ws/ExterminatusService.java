133b996026ed84dd2b85ca64685e5e28d8ab7cb4
Ivan Uskov <liveinkarai@gmail.com>
2019-02-10 15:06:18 +0300

    'update' method in exterminatus jaxws service.

--- exterminatus-service/src/main/java/ru/ifmo/wst/lab1/ws/ExterminatusService.java
+++ exterminatus-service/src/main/java/ru/ifmo/wst/lab1/ws/ExterminatusService.java
@@ -7,6 +7,7 @@ import ru.ifmo.wst.lab1.model.ExterminatusEntity;
 import javax.inject.Inject;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
+import javax.jws.WebResult;
 import javax.jws.WebService;
 import java.util.Date;
 import java.util.List;
@@ -42,6 +43,15 @@ public class ExterminatusService {
     @SneakyThrows
     public int delete(@WebParam(name = "id") long id) {
         return exterminatusDAO.delete(id);
+    }
+
+    @WebMethod
+    @SneakyThrows
+    @WebResult(name = "updatedCount")
+    public int update(@WebParam(name = "id") long id, @WebParam(name = "initiator") String initiator,
+                      @WebParam(name = "reason") String reason, @WebParam(name = "method") String method,
+                      @WebParam(name = "planet") String planet, @WebParam(name = "date") Date date) {
+        return exterminatusDAO.update(id, initiator, reason, method, planet, date);
     }
 
     public ExterminatusService(ExterminatusDAO exterminatusDAO) {
